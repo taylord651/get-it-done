f360b71bb99ec651a02f5984b90b4b90c94a0e13
Domonique Taylor <taylord651@gmail.com>
2019-07-14 09:53:25 -0500

    Final code with user-task relationship: user can only see their tasks

--- main.py
+++ main.py
@@ -14,16 +14,19 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     completed = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name):
+    def __init__(self, name, owner):
         self.name = name 
         self.completed = False
+        self.owner = owner
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+    tasks = db.relationship('Task', backref='owner')
 
     def __init__(self, email, password):
         self.email = email
@@ -81,14 +84,15 @@ def logout():
 @app.route('/', methods=["GET", "POST"])
 def index():
 
+    owner = User.query.filter_by(email=session['email']).first()
+
     if request.method == "POST":
         task_name = request.form['task']
-        new_task = Task(task_name)
+        new_task = Task(task_name, owner)
         db.session.add(new_task)
         db.session.commit()
 
-
-    tasks = Task.query.filter_by(completed=False).all()
+    tasks = Task.query.filter_by(completed=False,owner=owner).all()
     completed_tasks = Task.query.filter_by(completed=True).all()
     
     return render_template("todos.html", title="Get It Done!", 
